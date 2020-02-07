77a7e71591c0ac2f5b95155dd291d7037371341f
unknown <Viruraj16@gmail.com>
2020-02-07 11:30:21 +0530

    Files added

---
+++ JavaScanner/src/emp/EmpolyeeDetails.java
@@ -0,0 +1,40 @@
+package emp;
+
+import java.util.Scanner;
+
+public class EmpolyeeDetails {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int i, n=0, s1=0;
+		double avg;
+		{
+	
+		System.out.print("Input the 5 numbers: ");
+		}
+		for(i=1;i<=5;i++) {
+			System.out.println();
+			Scanner ss=new Scanner(System.in);
+			n=ss.nextInt();
+			s1 +=n;
+		} 
+		avg=n/5;
+		System.out.print("the sum of 5 number is: "+s1+"/nThe Average is: "+avg);
+		
+ 		
+ 		
+		//System.out.print("Enter the mark2");
+//		s.nextInt(); 
+// 		System.out.print("Enter the mark3");
+// 		c=s.next();
+// 		System.out.print("enter the mark4");
+// 		d=s.next();
+// 		System.out.print("enter the mark5");
+// 		e=s.next();
+// 		
+// 	System.out.println("total");
+// 	s.
+ 	
+ 		
+	}
+}
